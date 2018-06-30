3dafe7b0019f478887f00551db0249dc2ef6a808
mrachagung <vainfair@gmail.com>
2018-07-01 01:46:45 +0700

    add reset

--- main.go
+++ main.go
@@ -10,11 +10,17 @@ import (
 func main() {
 	db.InitDB()
 restart:
+	urlFb := "https://www.facebook.com/groups/532806206744264"
+	crawl.UrlFb = urlFb
 	res, err := crawl.RunCrawl()
 	if err != nil {
 		time.Sleep(3 * time.Second)
 		log.Println(err)
 		goto restart
 	}
-	crawl.Parse1(res)
+	reset := crawl.Parse1(res, urlFb)
+	if reset {
+		goto restart
+	}
+	log.Println("SUKSES PROSES SCARPING")
 }
